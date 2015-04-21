126c0590f0226d957b1287ab832b0d80b44bfa1f
Robotics Laptop <1768programming@gmail.com>
2015-04-21 00:27:56 -0400

    Added LED Link

---
+++ src/edu/nr/robotics/ArduinoLink.java
@@ -0,0 +1,59 @@
+package edu.nr.robotics;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class ArduinoLink 
+{
+	private static ArduinoLink instance;
+	public static ArduinoLink getInstance()
+	{
+		if(instance == null)
+			instance = new ArduinoLink();
+		return instance;
+	}
+	
+	private final String STATE_OFF = "Off",
+			STATE_COUNTDOWN = "Countdown",
+			STATE_SCORING = "Score";
+	
+	private String previousState = STATE_OFF;
+	private long startedScoringTime = 0;
+	
+	private String state = STATE_OFF;
+	private ArduinoLink()
+	{
+	}
+	
+	public void update()
+	{
+		int time = (int)Math.round(DriverStation.getInstance().getMatchTime());
+		
+		if(state.equals(STATE_SCORING))
+		{
+			if(System.currentTimeMillis() - startedScoringTime > 2000)
+				state = previousState;
+		}
+		
+		if(time <= 16)
+			state = STATE_COUNTDOWN;
+		
+		SmartDashboard.putString("ArduinoState", state);
+		
+		if(state.equalsIgnoreCase(STATE_COUNTDOWN))
+		{
+			SmartDashboard.putNumber("ArduinoArgument", time);
+		}
+		else
+		{
+			SmartDashboard.putNumber("ArduinoArgument", 0);
+		}
+	}
+	
+	public void scoreYo()
+	{
+		startedScoringTime = System.currentTimeMillis();
+		previousState = state;
+		state = STATE_SCORING;
+	}
+}
