e32d6336cee40ba398aea337582fe67601761b9c
Garrison Taylor <garrisontaylor@gmail.com>
2015-11-13 09:23:45 -0500

    Fix left and right setpoints being combined

--- src/edu/nr/robotics/subsystems/drive/commands/DrivePositionCommand.java
+++ src/edu/nr/robotics/subsystems/drive/commands/DrivePositionCommand.java
@@ -15,12 +15,14 @@ public class DrivePositionCommand extends CMD {
 	long startTime;
 	long prevTime;
 	long dt;
-	double errorLast;
+	double leftErrorLast;
+	double rightErrorLast;
 	
     public DrivePositionCommand(Position[] positions, double Kv, double Ka, double Kp, double Kd) {
         requires(Drive.getInstance());
         startTime = System.currentTimeMillis();
-        errorLast = 0;
+        rightErrorLast = 0;
+        leftErrorLast = 0;
         prevTime = startTime;
         this.Kv = Kv;
         this.Ka = Ka;
@@ -37,20 +39,36 @@ public class DrivePositionCommand extends CMD {
     // Called repeatedly when this Command is scheduled to run
     protected void onExecute() {
     	dt = System.currentTimeMillis() - prevTime;
-    	Setpoint setpoint = lookUpSetpoint(System.currentTimeMillis() - startTime);
-    	double error = setpoint.pos - FieldCentric.getInstance().getDistance();
-    	double error_deriv = (error - errorLast) / dt;
-    	double motorSpeed = Kv * setpoint.vel + Ka * setpoint.acc + Kp * error + Kd * error_deriv;
-    	Drive.getInstance().tankDrive(motorSpeed, motorSpeed);
     	prevTime = System.currentTimeMillis();
-    	errorLast = error;
+
+    	//Left wheel
+    	Setpoint leftSetpoint = lookUpLeftSetpoint(System.currentTimeMillis() - startTime);
+    	double leftError = leftSetpoint.pos - FieldCentric.getInstance().getDistance();
+    	double leftErrorDrive = (leftError - leftErrorLast) / dt;
+    	double leftMotorSpeed = Kv * leftSetpoint.vel + Ka * leftSetpoint.acc + Kp * leftError + Kd * leftErrorDrive;
+    	leftErrorLast = leftError;
+
+    	//Right wheel
+    	Setpoint rightSetpoint = lookUpRightSetpoint(System.currentTimeMillis() - startTime);
+    	double rightError = rightSetpoint.pos - FieldCentric.getInstance().getDistance();
+    	double rightErrorDeriv = (rightError - rightErrorLast) / dt;
+    	double rightMotorSpeed = Kv * rightSetpoint.vel + Ka * rightSetpoint.acc + Kp * rightError + Kd * rightErrorDeriv;
+    	rightErrorLast = rightError;
+    	
+    	Drive.getInstance().tankDrive(leftMotorSpeed, rightMotorSpeed);
     }
 
     private void createPath() {
     	// TODO determine the path the robot will take (a spline curve) using positions
     }
     
-    private Setpoint lookUpSetpoint(long t) {
+    private Setpoint lookUpLeftSetpoint(long t) {
+		// TODO get the pos, vel, and acc we should be at at time t
+    	double pos = 0, vel = 0, acc = 0;
+		return new Setpoint(pos,vel,acc);
+	}
+    
+    private Setpoint lookUpRightSetpoint(long t) {
 		// TODO get the pos, vel, and acc we should be at at time t
     	double pos = 0, vel = 0, acc = 0;
 		return new Setpoint(pos,vel,acc);
